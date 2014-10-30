f45138187822deecfa4dc09a53462bbab07a9ed8
Trevor Cooper <tcooper@sdsc.edu>
2014-10-30 08:33:35 -0700

    APS-38 Remove scar components from sdsc-roll
    The components of the scar-roll, added to the sdsc-roll, are
    not generic. They are either SDSC specific or the are scheduler
    specific. They do not belong in the sdsc-roll aka the 'shim' roll.

--- src/sdsc-etc-profile/sdsc.sh
+++ src/sdsc-etc-profile/sdsc.sh
@@ -7,25 +7,6 @@ for F in `find /opt/modulefiles -maxdepth 1 -mindepth 1 -type d`; do
   fi
 done
 
-# Point Python to sdsc library
-if test -z "${PYTHONPATH}"; then
-  export PYTHONPATH=/opt/sdsc/lib
-elif ! [[ "${PYTHONPATH}" =~ "/opt/sdsc/lib" ]]; then
-  export PYTHONPATH=/opt/sdsc/lib:$PYTHONPATH
-fi
-
-# Add sdsc scripts to path
-if test -z "${PATH}"; then
-  export PATH=/opt/sdsc/bin:/opt/sdsc/sbin
-else
-  if ! [[ "${PATH}" =~ "/opt/sdsc/bin" ]]; then
-    export PATH=/opt/sdsc/bin:$PATH
-  fi
-  if ! [[ "${PATH}" =~ "/opt/sdsc/sbin" ]]; then
-    export PATH=/opt/sdsc/sbin:$PATH
-  fi
-fi
-
 # Env vars to locate SDSC resources
 export SDSCHOME=/opt/sdsc
 export SDSCDEVEL=/opt/sdsc/devel
