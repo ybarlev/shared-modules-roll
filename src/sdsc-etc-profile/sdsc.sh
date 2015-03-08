6bb0c3aeaa09125feae9dd9488d43931fe89ffc3
ybarlev <yevgeny.barlev@gmail.com>
2015-03-07 20:58:37 -0500

    Update sdsc.sh

--- src/sdsc-etc-profile/sdsc.sh
+++ src/sdsc-etc-profile/sdsc.sh
@@ -1,36 +1,6 @@
-# Add /opt/modulefiles/* to module search path
-for F in `find /opt/modulefiles -maxdepth 1 -mindepth 1 -type d`; do
+# Add /share/apps/modulefiles to module search path
   if test -z "$MODULEPATH"; then
-    export MODULEPATH=${F}
-  elif ! [[ "${MODULEPATH}" =~ "${F}" ]]; then
-    export MODULEPATH=${MODULEPATH}:${F}
+    export MODULEPATH=/share/apps/modulefiles
+  else
+    export MODULEPATH=${MODULEPATH}:/share/apps/modulefiles
   fi
-done
-
-# Add sdsc libraries to search paths
-if test -z "${PYTHONPATH}"; then
-  export PYTHONPATH=/opt/sdsc/lib
-elif ! [[ "${PYTHONPATH}" =~ "/opt/sdsc/lib" ]]; then
-  export PYTHONPATH=${PYTHONPATH}:/opt/sdsc/lib
-fi
-if test -z "${LD_LIBRARY_PATH}"; then
-  export LD_LIBRARY_PATH=/opt/sdsc/lib
-elif ! [[ "${LD_LIBRARY_PATH}" =~ "/opt/sdsc/lib" ]]; then
-  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/sdsc/lib
-fi
-
-# Add sdsc scripts to path
-if test -z "${PATH}"; then
-  export PATH=/opt/sdsc/bin:/opt/sdsc/sbin
-else
-  if ! [[ "${PATH}" =~ "/opt/sdsc/bin" ]]; then
-    export PATH=${PATH}:/opt/sdsc/bin
-  fi
-  if ! [[ "${PATH}" =~ "/opt/sdsc/sbin" ]]; then
-    export PATH=${PATH}:/opt/sdsc/sbin
-  fi
-fi
-
-# Env vars to locate SDSC resources
-export SDSCHOME=/opt/sdsc
-export SDSCDEVEL=/opt/sdsc/devel
